b8778f61097fd2eb2014bac7a284febe4306ad65
kiddo27 <braveninja227@gmail.com>
2026-03-15 05:54:51 +0700

    initalize repo

---
+++ Dasar C++/perkenalan_soal_cp_2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+
+    int N;
+    cin >> N;
+    vector <long long int> A;
+    long long int jumlah = 0;
+
+    for(int i = 0; i < N; i++){
+        int x;
+        cin >> x;
+        A.push_back(x);
+    }
+
+    for(int i = 0; i < N; i++){
+
+        jumlah = jumlah + A[i];
+    }
+
+    for(int i = 0; i < N; i++){
+        cout << jumlah - A[i] << endl;
+    }
+
+
+}
