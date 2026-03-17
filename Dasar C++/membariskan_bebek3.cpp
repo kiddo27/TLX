ccb23824383ee4a4dce3c1f6181b72bc2ecced60
kiddo27 <braveninja227@gmail.com>
2026-03-17 09:29:43 +0700

    membariskan bebek 3

--- Dasar C++/membariskan_bebek3.cpp
+++ Dasar C++/membariskan_bebek3.cpp
@@ -1,23 +1,27 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
+
 
 int main(){
 
-    int N;
-    int r_terbaik = 1;
+    long long int N;
+    long long int c;
+    long long int r_terbaik = 1;
     cin >> N;
     
-    int c_terbaik = N;
-    for(int r = 1; r <= N; r++){
+    long long int c_terbaik = N;
+    for(long long int r = 1; r <= sqrt(N); r++){
         if(N%r == 0){
-            int c = N/r;
+            c =N/r;
             if(r<=c){
             if((c - r) < (c_terbaik - r_terbaik)){
                 r_terbaik = r;
                 c_terbaik = c;
             }
         }
-       }
+            
+        }
     }
 
     cout << r_terbaik << " " << c_terbaik << endl;
